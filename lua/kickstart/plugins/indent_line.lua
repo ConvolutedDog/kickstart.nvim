02699ca194847e625b2f128efd3e5c0c0c1feaba
JIAN CHAO YANG <yangjianchao16@nudt.edu.cn>
2024-12-22 15:14:32 +0800

    Update indent_line.lua

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,6 +1,6 @@
 return {
   { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
+    'https://gitee.com/masa-laboratory/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
