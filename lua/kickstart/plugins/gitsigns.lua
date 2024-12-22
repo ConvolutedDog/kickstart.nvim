83e0bf664327f4676e3b231f4f10fdb4213de955
JIAN CHAO YANG <yangjianchao16@nudt.edu.cn>
2024-12-22 15:14:42 +0800

    Update gitsigns.lua

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -4,7 +4,7 @@
 
 return {
   {
-    'lewis6991/gitsigns.nvim',
+    'https://gitee.com/masa-laboratory/gitsigns.nvim',
     opts = {
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
